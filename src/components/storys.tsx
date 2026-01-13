96a2e70f6f0063e154889ff1f470fb724be2b33c
sobhan <sobhanjafarii87@gmail.com>
2026-01-13 20:59:05 +0330

    added-after-before-icons

--- src/components/storys.tsx
+++ src/components/storys.tsx
@@ -2,29 +2,42 @@ import useStorys from "@/service/Header/Header-Storys/hook";
 import Story from "./story";
 import { Istory } from "@/service/Header/Header-Storys/types";
 
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
 function Storys() {
   const { data = [], isError, isFetching } = useStorys();
 
   return (
-    <div
-      className="hidden lg:flex justify-start items-center h-40 my-5 gap-3 lg:container overflow-auto scrollbar-hide m-auto w-7/10"
-      dir="rtl"
-    >
-      {isFetching && (
-        <h2 className="m-auto font-bold text-2xl" dir="ltr">
-          loading ...
-        </h2>
-      )}
+    <div className="hidden lg:flex justify-between items-center relative">
+      <section className="py-2 pr-1 pl-3.5 bg-white rounded-full cursor-pointer active:opacity-40 Shadow absolute top-15 left-25">
+        <ArrowBackIosIcon />
+      </section>
 
-      {isError && (
-        <h2 className="m-auto font-bold text-2xl">
-          data not found please reload again
-        </h2>
-      )}
+      <div
+        className="flex justify-start items-center h-40 my-5 gap-3 lg:container overflow-auto scrollbar-hide m-auto w-7/10"
+        dir="rtl"
+      >
+        {isFetching && (
+          <h2 className="m-auto font-bold text-2xl" dir="ltr">
+            loading ...
+          </h2>
+        )}
 
-      {!isFetching &&
-        !isError &&
-        data.map((item: Istory) => <Story key={item.id} {...item} />)}
+        {isError && (
+          <h2 className="m-auto font-bold text-2xl">
+            data not found please reload again
+          </h2>
+        )}
+
+        {!isFetching &&
+          !isError &&
+          data.map((item: Istory) => <Story key={item.id} {...item} />)}
+      </div>
+
+      <section className="py-2 pl-2.5 pr-2.25 bg-white rounded-full cursor-pointer active:opacity-40 Shadow absolute top-15 right-25">
+        <ArrowForwardIosIcon />
+      </section>
     </div>
   );
 }
