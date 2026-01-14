d4c1c5ebdf8afaf9b1ed3cc4336ddf415a54e6c8
sobhan <sobhanjafarii87@gmail.com>
2026-01-14 14:20:30 +0330

    finished-storys-section

--- src/components/storys.tsx
+++ src/components/storys.tsx
@@ -10,12 +10,12 @@ function Storys() {
 
   return (
     <div className="hidden lg:flex justify-between items-center relative">
-      <section className="py-2 pr-1 pl-3.5 bg-white rounded-full cursor-pointer active:opacity-40 Shadow absolute top-15 left-25">
+      <section className="py-2 pr-1 pl-3.5 bg-white rounded-full text-gray-700 cursor-pointer active:opacity-40 Shadow absolute top-15 left-50">
         <ArrowBackIosIcon />
       </section>
 
       <div
-        className="flex justify-start items-center h-40 my-5 gap-3 lg:container overflow-auto scrollbar-hide m-auto w-7/10"
+        className="flex justify-start items-center h-40 my-5 gap-1 lg:container overflow-auto scrollbar-hide m-auto w-7/10"
         dir="rtl"
       >
         {isFetching && (
@@ -35,7 +35,7 @@ function Storys() {
           data.map((item: Istory) => <Story key={item.id} {...item} />)}
       </div>
 
-      <section className="py-2 pl-2.5 pr-2.25 bg-white rounded-full cursor-pointer active:opacity-40 Shadow absolute top-15 right-25">
+      <section className="py-2 pl-2.5 pr-2.25 bg-white rounded-full text-gray-700 cursor-pointer active:opacity-40 Shadow absolute top-15 right-50">
         <ArrowForwardIosIcon />
       </section>
     </div>
