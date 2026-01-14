42e037f738e209e35498298897fb74dd6b3ce29a
sobhan <sobhanjafarii87@gmail.com>
2026-01-14 21:39:49 +0330

    edit-headerNavBar-component

--- src/components/OfferItem.tsx
+++ src/components/OfferItem.tsx
@@ -1,7 +1,11 @@
 function OfferItem({ imgUrl, content }: { imgUrl: string; content: string }) {
   return (
-    <div className="flex flex-col justify-start items-center w-20 h-full cursor-pointer">
-      <img src={imgUrl} alt="OfferImg" className="rounded-full w-15 h-15" />
+    <div className="flex flex-col justify-start items-center min-w-20 w-20 h-full cursor-pointer">
+      <img
+        src={imgUrl}
+        alt="OfferImg"
+        className="rounded-full w-10 h-10 lg:w-15 lg:h-15"
+      />
 
       <p className="text-center text-[12px] max-w-16 mt-2">{content}</p>
     </div>
--- src/components/headerNavBar.tsx
+++ src/components/headerNavBar.tsx
@@ -4,8 +4,42 @@ import MenuIcon from "@mui/icons-material/Menu";
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import LocalMallIcon from "@mui/icons-material/LocalMall";
 import WhatshotIcon from "@mui/icons-material/Whatshot";
+import { ReactElement } from "react";
+
+interface Idata {
+  id: number;
+  title: string;
+  icon?: ReactElement<any, string>;
+}
 
 function HeaderNavBar() {
+  const data: Idata[] = [
+    {
+      id: 1,
+      title: "شگفت انگیزها",
+      icon: <LocalOfferIcon fontSize="small" />,
+    },
+    {
+      id: 2,
+      title: "سوپرمارکت",
+      icon: <LocalMallIcon fontSize="small" />,
+    },
+    {
+      id: 3,
+      title: "طلای دیجیتال",
+      icon: <i className="fa-solid fa-coins"></i>,
+    },
+    {
+      id: 4,
+      title: "پرفروش ترین ها",
+      icon: <WhatshotIcon fontSize="small" />,
+    },
+    {
+      id: 5,
+      title: "!دردیجی کالابفروشید",
+    },
+  ];
+
   return (
     <div>
       <div className="m-4 text-gray-900 flex justify-end items-center text-[12px]">
@@ -17,26 +51,16 @@ function HeaderNavBar() {
 
         <div className="hidden lg:flex justify-end items-center w-[76%]">
           <ul className="text-gray-700 mr-5 flex flex-row-reverse justify-between items-center gap-4">
-            <li className="flex justify-between items-center">
-              شگفت انگیزها
-              <LocalOfferIcon fontSize="small" />
-            </li>
-            <li className="flex justify-between items-center">
-              سوپرمارکت
-              <LocalMallIcon fontSize="small" />
-            </li>
-            <li className="flex justify-between items-center">
-              طلای دیجیتال
-              <i className="fa-solid fa-coins"></i>
-            </li>
-            <li className="flex justify-between items-center">
-              پرفروش ترین ها
-              <WhatshotIcon fontSize="small" />
-            </li>
-            |<li className="flex justify-between items-center">سوالی دارید؟</li>
-            <li className="flex justify-between items-center">
-              !دردیجی کالابفروشید
-            </li>
+            {data.map((item: Idata) => (
+              <li
+                className="flex justify-between items-center cursor-pointer"
+                key={item.id}
+              >
+                <p>{item.title}</p>
+
+                {item?.icon}
+              </li>
+            ))}
           </ul>
           |
           <div className="flex justify-end items-center ml-5">
