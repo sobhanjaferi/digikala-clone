83c00b8bc6648ec626d346105c5bfc82ca2e31a4
sobhan <sobhanjafarii87@gmail.com>
2026-01-19 22:15:17 +0330

    added-loading-page

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="w-screen h-screen bg-gray-600/70 flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-white">Loading Page...</h1>
+    </div>
+  );
+}
+
+export default Loading;
