8f69ac4f050d0fff8e190415b8c331b23c16654e
sobhan <sobhanjafarii87@gmail.com>
2026-01-21 01:48:50 +0330

    developed-second-chaptear

--- src/components/chapters.tsx
+++ src/components/chapters.tsx
@@ -1,12 +1,22 @@
 import Chaptear from "./chapter";
 
-function Chaptears() {
+function Chaptears({
+  imgOne,
+  imgTwo,
+  imgThree,
+  imgFour,
+}: {
+  imgOne: string;
+  imgTwo: string;
+  imgThree: string;
+  imgFour: string;
+}) {
   return (
     <div className="m-5 lg:mt-0 grid grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-4 w-full lg:w-310">
-      <Chaptear imgUrl="https://dkstatics-public.digikala.com/digikala-adservice-banners/ad775f65805c15a4267818ed51e5aff5f1943446_1766330967.jpg?x-oss-process=image/quality,q_95" />
-      <Chaptear imgUrl="https://dkstatics-public.digikala.com/digikala-adservice-banners/28aace2f95904816966dafce7c9eda9edb9c2026_1761132457.jpg?x-oss-process=image/quality,q_95" />
-      <Chaptear imgUrl="https://dkstatics-public.digikala.com/digikala-adservice-banners/847e655fab88fe48f4026900508b6a0af8cf9685_1768298491.jpg?x-oss-process=image/quality,q_95" />
-      <Chaptear imgUrl="https://dkstatics-public.digikala.com/digikala-adservice-banners/04757f3ef6dc062163e3e5bc1879b7025e098b43_1766330030.jpg?x-oss-process=image/quality,q_95" />
+      <Chaptear imgUrl={imgOne} />
+      <Chaptear imgUrl={imgTwo} />
+      <Chaptear imgUrl={imgThree} />
+      <Chaptear imgUrl={imgFour} />
     </div>
   );
 }
