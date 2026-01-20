f6483d9942892d7cff73f1c60128044827e9eeb9
sobhan <sobhanjafarii87@gmail.com>
2026-01-20 12:50:01 +0330

    added-scroll-button-to-storys

--- src/components/storys.tsx
+++ src/components/storys.tsx
@@ -5,17 +5,38 @@ import { Istory } from "@/service/Header/Header-Storys/types";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import LoadingAndError from "./LoadingAndError";
+import { useRef } from "react";
 
 function Storys() {
   const { data = [], isError, isFetching, isPending } = useStorys();
 
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const HandleScrollLeft = () => {
+    scrollRef.current?.scrollBy({
+      left: 300,
+      behavior: "smooth",
+    });
+  };
+
+  const HandleScrollRight = () => {
+    scrollRef.current?.scrollBy({
+      left: -300,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="hidden lg:flex justify-between items-center relative">
-      <section className="py-2 pr-1 pl-3.5 bg-white rounded-full text-gray-700 cursor-pointer active:opacity-40 Shadow absolute top-15 left-50">
+      <section
+        onClick={HandleScrollRight}
+        className="py-2 pr-1 pl-3.5 bg-white rounded-full text-gray-700 cursor-pointer active:opacity-40 Shadow absolute top-15 left-50"
+      >
         <ArrowBackIosIcon />
       </section>
 
       <div
+        ref={scrollRef}
         className="flex justify-start items-center h-40 my-5 gap-1 lg:container overflow-auto scrollbar-hide m-auto w-7/10"
         dir="rtl"
       >
@@ -30,7 +51,10 @@ function Storys() {
           data.map((item: Istory) => <Story key={item.id} {...item} />)}
       </div>
 
-      <section className="py-2 pl-2.5 pr-2.25 bg-white rounded-full text-gray-700 cursor-pointer active:opacity-40 Shadow absolute top-15 right-50">
+      <section
+        onClick={HandleScrollLeft}
+        className="py-2 pl-2.5 pr-2.25 bg-white rounded-full text-gray-700 cursor-pointer active:opacity-40 Shadow absolute top-15 right-50"
+      >
         <ArrowForwardIosIcon />
       </section>
     </div>
