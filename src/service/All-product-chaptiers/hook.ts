e872ae5ee329e4ac61d4bec61c10b2031f0eda11
sobhan <sobhanjafarii87@gmail.com>
2026-02-02 10:46:56 +0330

    get-data-from-API

---
+++ src/service/All-product-chaptiers/hook.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import IproductChaptier from "./types";
+import axios from "axios";
+
+export default function useAllProductChaptiers() {
+  return useQuery<IproductChaptier[]>({
+    queryKey: ["AllProductChaptiers"],
+    queryFn: async () => {
+      const { data } = await axios("http://localhost:8000/AllProductChaptiers");
+
+      return data as IproductChaptier[];
+    },
+  });
+}
