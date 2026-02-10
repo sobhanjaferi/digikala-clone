9b7602b923f18e77b9155e0ffcdb6522542e02b4
sobhan <sobhanjafarii87@gmail.com>
2026-02-10 10:53:09 +0330

    added-popular-brands-item

---
+++ src/components/PopularBrand.tsx
@@ -0,0 +1,7 @@
+function PopularBrand() {
+  return (
+    <div className="w-30 min-w-30 h-full bg-red-500 rounded-lg flex flex-col justify-between items-center"></div>
+  );
+}
+
+export default PopularBrand;
