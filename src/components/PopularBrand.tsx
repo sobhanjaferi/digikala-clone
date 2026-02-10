e6c4f3989846990a847b77821d592214dd379d73
sobhan <sobhanjafarii87@gmail.com>
2026-02-10 11:12:16 +0330

    added-responsive-popular-brands-item

--- src/components/PopularBrand.tsx
+++ src/components/PopularBrand.tsx
@@ -1,6 +1,10 @@
 function PopularBrand() {
   return (
-    <div className="w-30 min-w-30 h-full bg-red-500 rounded-lg flex flex-col justify-between items-center"></div>
+    <div className="min-w-25 lg:min-w-30 lg:border-0 border border-gray-400 h-full rounded-lg flex flex-col justify-between items-center">
+      <div className="rounded-lg w-full h-8/10 lg:h-full">img</div>
+
+      <h2 className="h-2/10 lg:hidden">title</h2>
+    </div>
   );
 }
 
