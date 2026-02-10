ef894160be82f52a3fb2b7293f3d8c2a7f4de467
sobhan <sobhanjafarii87@gmail.com>
2026-02-10 11:22:37 +0330

    use-data-for-popular-brand-items

--- src/components/PopularBrand.tsx
+++ src/components/PopularBrand.tsx
@@ -1,9 +1,11 @@
-function PopularBrand() {
-  return (
-    <div className="min-w-25 lg:min-w-30 lg:border-0 border border-gray-400 h-full rounded-lg flex flex-col justify-between items-center">
-      <div className="rounded-lg w-full h-8/10 lg:h-full">img</div>
+import IpopularBrand from "@/service/Popular-Brands/types";
 
-      <h2 className="h-2/10 lg:hidden">title</h2>
+function PopularBrand({ imgUrl, title }: IpopularBrand) {
+  return (
+    <div className="min-w-25 lg:min-w-30 lg:border-0 border border-gray-400 h-full rounded-lg flex flex-col justify-between items-center p-2">
+      <img src={imgUrl} className="rounded-sm w-full h-8/10 lg:h-full" />
+
+      <h2 className="h-2/10 lg:hidden">{title}</h2>
     </div>
   );
 }
--- src/components/PopularBrands.tsx
+++ src/components/PopularBrands.tsx
@@ -1,38 +1,28 @@
+"use client";
+
 import StarOutlineIcon from "@mui/icons-material/StarOutline";
 import PopularBrand from "./PopularBrand";
+import usePopularBrans from "@/service/Popular-Brands/hook";
+import IpopularBrand from "@/service/Popular-Brands/types";
 
 function PopularBrands() {
+  const { data = [] } = usePopularBrans();
+
   return (
-    <div className="w-full lg:container lg:mx-auto mx-5 mt-5 mb-10 flex flex-col justify-end items-end">
-      <section className="flex flex-row-reverse mb-5">
+    <div className="lg:container lg:mx-auto mx-5 mt-5 mb-10 flex flex-col justify-end items-end">
+      <section className="flex flex-row-reverse mb-5 mr-5">
         <StarOutlineIcon />
 
         <h1>محبوب ترین برندها</h1>
       </section>
 
-      <div className="h-42 w-full overflow-auto flex p-5 gap-5" dir="rtl">
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
-        <PopularBrand />
+      <div
+        className="h-42 w-full overflow-auto scrollbar-hide flex p-5 gap-5"
+        dir="rtl"
+      >
+        {data.map((item: IpopularBrand) => (
+          <PopularBrand key={item.id} {...item} />
+        ))}
       </div>
     </div>
   );
