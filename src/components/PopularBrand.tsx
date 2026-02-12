4c33c460a28345ce416a618021e39ee7d13290d3
sobhan <sobhanjafarii87@gmail.com>
2026-02-12 15:23:42 +0330

    edited-popular-brand-items-styles

--- src/components/PopularBrand.tsx
+++ src/components/PopularBrand.tsx
@@ -1,9 +1,12 @@
 import IpopularBrand from "@/service/Popular-Brands/types";
 
-function PopularBrand({ imgUrl, title }: IpopularBrand) {
+function PopularBrand({ imgUrl, title, id }: IpopularBrand) {
   return (
     <div className="min-w-25 lg:min-w-30 lg:border-0 border border-gray-400 h-full rounded-lg flex flex-col justify-between items-center p-2">
-      <img src={imgUrl} className="rounded-sm w-full h-8/10 lg:h-full" />
+      <img
+        src={imgUrl}
+        className={`rounded-sm w-full h-8/10 ${id == 13 || id == 14 ? "lg:h-4/10 mt-7" : "lg:h-full"}`}
+      />
 
       <h2 className="h-2/10 lg:hidden">{title}</h2>
     </div>
