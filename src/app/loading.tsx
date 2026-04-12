1f93d6976a054598c885cea88108825c271dd9d1
sobhan <sobhanjafarii87@gmail.com>
2026-04-12 11:20:41 +0330

    added-animations-t-loading-page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,9 @@
 function Loading() {
   return (
-    <div className="w-screen h-screen bg-gray-600/70 flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-white">Loading Page...</h1>
+    <div className="w-screen h-screen bg-gray-600/70 flex justify-center items-center loadingAnimation">
+      <h1 className="text-3xl font-bold text-white loadingTextAnimation">
+        Loading Page...
+      </h1>
     </div>
   );
 }
