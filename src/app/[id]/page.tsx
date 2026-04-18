62a251f961bcc99591f740c6f7b051bfd702975d
sobhan <sobhanjafarii87@gmail.com>
2026-04-18 19:53:34 +0330

    developed-add-to-cart-section

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,9 +1,10 @@
-/* =============== Import Sections =============== */
+/* =============== Import Section =============== */
 import Header from "@/components/HeaderMobileSlider";
 import OfferSliderHeader from "@/components/OfferSliderHeader";
 import OfferSliderPictures from "@/components/OfferSliderPictures";
 import OfferSliderProductInfo from "@/components/OfferSliderProductInfo";
 import OfferSliderProductOfferTimer from "@/components/OfferSliderProductOfferTimer";
+import ProductAddToCart from "@/components/productAddToCart";
 import { IofferSlide } from "@/service/Offer-Slider/types";
 import axios from "axios";
 
@@ -42,6 +43,12 @@ async function Product({ params }: Tprops) {
         {/* =============== Product Information  =============== */}
         <OfferSliderProductInfo data={data} />
       </main>
+
+      <footer className="fixed left-0 right-0 bottom-0 p-5 bg-white border-t border-gray-300 flex flex-row-reverse justify-between items-center">
+        {/* =============== Add To Cart Section  =============== */}
+
+        <ProductAddToCart data={data} />
+      </footer>
     </>
   );
 }
